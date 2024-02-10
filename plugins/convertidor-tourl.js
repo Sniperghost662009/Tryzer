a4031e1c8afa1f626da4c23c4ff53cb422c1a27e
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-02-10 00:20:53 -0600

    Create convertidor-tourl.js

---
+++ plugins/convertidor-tourl.js
@@ -0,0 +1,15 @@
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ᴏ ᴠɪᴅᴇᴏ ᴇʟ ᴄᴜᴀʟ ꜱᴇʀᴀ ᴄᴏɴᴠᴇʀᴛɪᴅᴏ ᴀ ᴇɴʟᴀᴄᴇ*';
+  const media = await q.download();
+  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const link = await (isTele ? uploadImage : uploadFile)(media);
+  m.reply(`*🐶 ᴇɴʟᴀᴄᴇ ᴀ ꜱᴜ ᴀʀᴄʜɪᴠᴏ:* ${link}`);
+};
+handler.help = ['tourl <reply image>'];
+handler.tags = ['tools'];
+handler.command = /^(upload|tourl)$/i;
+export default handler;
