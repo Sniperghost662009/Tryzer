7968980890a25981023c98d942ef33b4c38940ee
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-02-11 15:00:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,88 @@
+console.log('✅ Iniciando...')
 
-console.log('Iniciando...');
-import {join, dirname} from 'path';
-import {createRequire} from 'module';
-import {fileURLToPath} from 'url';
-import {setupMaster, fork} from 'cluster';
-import {watchFile, unwatchFile} from 'fs';
+import { join, dirname } from 'path'
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
-import {createInterface} from 'readline';
-import yargs from 'yargs';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const {name, author} = require(join(__dirname, './package.json'));
-const {say} = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+import express from 'express'
+import chalk from 'chalk'
+import path from 'path'
+import os from 'os'
+import { promises as fsPromises } from 'fs'
 
-say('Tryzer', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']});
-say(`bot by als`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['blue', 'magenta']});
+// https://stackoverflow.com/a/50052194
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
 
-let isRunning = false;
-/**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
-function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
+const app = express()
+const port = process.env.PORT || 8080;
 
-  /** say('[ ℹ️ ] Escanea el código QR o introduce el código de emparejamiento en WhatsApp.', {
+say('Tryzer- MD', {
+font: 'chrome',
+align: 'center',
+gradient: ['red', 'magenta']})
+say(`Simple WhatsApp Bot Multi Device`, {
+font: 'console',
+align: 'center',
+gradient: ['red', 'magenta']})
+
+var isRunning = false
+
+async function start(file) {
+  if (isRunning) return
+  isRunning = true
+  const currentFilePath = new URL(import.meta.url).pathname
+  let args = [join(__dirname, file), ...process.argv.slice(2)]
+  say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
-    gradient: ['red', 'magenta']}); **/
-
+    gradient: ['red', 'magenta']
+  })
   setupMaster({
     exec: args[0],
-    args: args.slice(1)});
-  const p = fork();
-  p.on('message', (data) => {
-    console.log('[RECIBIDO]', data);
+    args: args.slice(1),
+  })
+  let p = fork()
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
     switch (data) {
       case 'reset':
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
+        p.process.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
       case 'uptime':
-        p.send(process.uptime());
-        break;
+        p.send(process.uptime())
+        break
     }
-  });
+  })
+  //---
   p.on('exit', (_, code) => {
-    isRunning = false;
-    console.error('Ocurrio un error inesperado:', code);
+    isRunning = false
+    console.error('❎ Ocurrió un error inesperado:', code)
+    start('main.js'); //
 
-    p.process.kill();
-    isRunning = false;
-    start.apply(this, arguments);
-
-    if (process.env.pm_id) {
-      process.exit(1);
-    } else {
-      process.exit();
-    }
-  });
-  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-  if (!opts['test']) {
-    if (!rl.listenerCount()) {
-      rl.on('line', (line) => {
-        p.emit('message', line.trim());
-      });
-    }
-  }
+    if (code === 0) return
+    watchFile(args[0], () => {
+      unwatchFile(args[0])
+      start(file)
+    })
+  })
+  
+  //----
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  if (!opts['test'])
+    if (!rl.listenerCount()) rl.on('line', line => {
+      p.emit('message', line.trim())
+    })
+  // console.log(p)
 }
-start('main.js');
+
+start('main.js')
